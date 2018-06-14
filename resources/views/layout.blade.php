a0794d241aeced76449d644aca3593d40588ec3e
EikiKanaoka <kakakaeee@gmail.com>
2018-06-14 21:10:12 +0900

    bladeでレイアウトを作る

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>Sample</title>
+  </head>
+  <body>
+      @yield('content')
+  </body>
+</html>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,13 +1,18 @@
 <!-- app/resources/view/contact.blade.php -->
-<!DOCTYPE HTML>
+<!-- <!DOCTYPE HTML>
 <html>
 <head>
     <title>Pages About</title>
 </head>
-<body>
-    <h1>About me!</h1>
+<body> -->
+    <!-- <h1>About me!</h1> -->
     <!-- <h2>Hello <?= $first_name ?> <?= $last_name ?> ! (PHPタグを使って変数表示)</h2> -->
-    <h2>Hello {{ $first_name }} {{ $last_name }} ! (blade書式を使って変数表示)</h2>
+    <!-- <h2>Hello {{ $first_name }} {{ $last_name }} ! blade書式を使って変数表示</h2> -->
+<!-- </body>
+</html> -->
 
-</body>
-</html>
+<!-- @extandsで'layout.blade.php'を継承 -->
+@extends('layout')
+@section('content')
+  <h1>About me : {{ $first_name }} {{ $last_name }}</h1>
+@endsection
